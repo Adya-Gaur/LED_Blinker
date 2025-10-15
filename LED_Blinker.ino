1c1f358093dbe3156de587d6adef05706e858751
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-15 21:06:04 +0530

    Update LED_Blinker.ino

--- LED_Blinker.ino
+++ LED_Blinker.ino
@@ -1,16 +1,18 @@
-//Default
-//Using the inbuilt LED on Arduino Board
-void setup()
+//Default-Using the inbuilt LED on Arduino Board
+void setup() //this function runs only once when the Arduino board is started or reset
 {
-  //initializing the LED on arduino Board as output
+  //pinMode- tells Arduino whether a pin is going to recieve a signal(INPUT from sensors) or send a signal(OUTPUT to actuators). 
+  //Initializing the LED on arduino Board as output
   pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop()
 {
   //infinite loop
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
+  //digitalWrite - function for sending signals(Digital)
+  //dugitalRead(pin) and analogRead(pin) are for receiving signals
+  digitalWrite(LED_BUILTIN, HIGH); //HIGH => High voltage => LED glows
+  delay(1000); // LED glows for 1 second. 1000ms = 1s
+  digitalWrite(LED_BUILTIN, LOW); //LOW => Low voltage => LED turns off
   delay(1000);
 }
 //LED remains on for 1 second and then turns off for a second 
