69576d688ac89f4a1d2cce12127f7020b3914d22
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-15 20:45:11 +0530

    Update LED_Blinker.ino

--- LED_Blinker.ino
+++ LED_Blinker.ino
@@ -1,1 +1,16 @@
-
+//Default
+//Using the inbuilt LED on Arduino Board
+void setup()
+{
+  //initializing the LED on arduino Board as output
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop()
+{
+  //infinite loop
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
+//LED remains on for 1 second and then turns off for a second 
