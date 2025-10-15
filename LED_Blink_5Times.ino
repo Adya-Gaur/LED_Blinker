a4d53ce977ab6855ba0ce9d1d6e805cbba4f0e47
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-15 22:02:17 +0530

    Update LED_Blink_5Times.ino

--- LED_Blink_5Times.ino
+++ LED_Blink_5Times.ino
@@ -1,2 +1,19 @@
+//In this modification, LED blinks 5 times and then remains off
 void setup()
 {
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop()
+{
+  for(int i=1; i<=5; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+  } 
+  //After Blinking 5 times, stop doing anything
+  while(true)
+  { //This empty loop keeps the program running forever without repeating the blinks
+  }
+}
