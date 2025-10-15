3467455f8619006c700c48e375b0f17a12c3e0aa
Adya Gaur <adyagaurofficial@gmail.com>
2025-10-15 21:31:28 +0530

    Update LED_Blinker_Fast.ino

--- LED_Blinker_Fast.ino
+++ LED_Blinker_Fast.ino
@@ -1,2 +1,21 @@
 //Modifying the default 1 second LED blinker. 
-//LED blinks fast, turns off for 2 seconds then remains on for 2 seconds
+//LED blinks fast, remains on for 3 seconds then turns off for 2 seconds. This continues till the program runs.
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+void loop()
+{
+  for(int i=1; i<=5; i++)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    //This loop turns led on and off 5 times for 0.5s each time
+  }
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(3000); //Keeps LED on for 3s
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(2000); //Keeps LED off for 2s
+}
